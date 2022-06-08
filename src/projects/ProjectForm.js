3aa1d96b4331e959fa1bca0630ffaf8c64e625f4
Michael Adebambo <80793630+Blazing-Mike@users.noreply.github.com>
2022-06-08 23:13:29 +0100

    implemented cancle button and save form
    Component Communication through Multiple Levels

--- src/projects/ProjectForm.js
+++ src/projects/ProjectForm.js
@@ -2,9 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Project } from "./Project";
 
-function ProjectForm() {
+  function ProjectForm({ onSave, onCancle }) {
+   const handleSubmit = (event) => {
+     event.preventDefault();
+     onSave(new Project({ name: 'Updated Project' }));
+   };
     return (
-    <form className="input-group vertical">
+    <form className="input-group vertical" onSubmit={handleSubmit}>
   <label htmlFor="name">Project Name</label>
   <input type="text" name="name" placeholder="enter name" />
   <label htmlFfor="description">Project Description</label>
@@ -19,11 +23,16 @@ function ProjectForm() {
   <div className="input-group">
     <button className="primary bordered medium">Save</button>
     <span></span>
-    <button type="button" className="bordered medium">cancel</button>
+    <button type="button" className="bordered medium" onClick ={onCancle}>cancel</button>
   </div>
 </form>
 );
+  }
 
 
-}
+ProjectForm.propTypes = {
+  onCancle: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired
+};
+
 export default ProjectForm;
--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -4,16 +4,19 @@ import PropTypes from "prop-types";
 import { Project } from "./Project";
 import ProjectForm from "./ProjectForm";
 
-function ProjectList({ projects }) {
+function ProjectList({ projects, onSave }) {
   const [projectBeingEdited, setProjectBeingEdited] = useState({});
   const handleEdit = (project) => {
     setProjectBeingEdited(project);
+  };
+  const cancelEditing = () => {
+    setProjectBeingEdited({});
   };
 
   const items = projects.map((project) => (
     <div key={project.id} className="cols-sm">
       {project === projectBeingEdited ? (
-        <ProjectForm />
+        <ProjectForm onSave={onSave} onCancle={cancelEditing} />
       ) : (
         <ProjectCard project={project} onEdit={handleEdit} />
       )}
@@ -24,6 +27,7 @@ function ProjectList({ projects }) {
 
 ProjectList.propTypes = {
   projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+  onSave: PropTypes.func.isRequired,
 };
 
 export default ProjectList;
