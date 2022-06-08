9c85e6034f739d9e8870346210e3d88ec3b769d9
Michael Adebambo <80793630+Blazing-Mike@users.noreply.github.com>
2022-06-08 22:45:19 +0100

    use useState hook and conditional rendering
    use useState hook and conditional rendering to show an element when the other is not visible

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import ProjectCard from './ProjectCard';
-import PropTypes from 'prop-types';
-import { Project } from './Project';
-import ProjectForm from './ProjectForm';
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import PropTypes from "prop-types";
+import { Project } from "./Project";
+import ProjectForm from "./ProjectForm";
 
- function ProjectList({ projects }) {
-    const handleEdit  = (project) => {
-       console.log(project);
+function ProjectList({ projects }) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+  const handleEdit = (project) => {
+    setProjectBeingEdited(project);
+  };
 
-    };
+  const items = projects.map((project) => (
+    <div key={project.id} className="cols-sm">
+      {project === projectBeingEdited ? (
+        <ProjectForm />
+      ) : (
+        <ProjectCard project={project} onEdit={handleEdit} />
+      )}
+    </div>
+  ));
+  return <div className="row">{items}</div>;
+}
 
-    const items = projects.map(project => (
-        <div key={project.id} className="cols-sm">
-            <ProjectCard project={project} onEdit={handleEdit}></ProjectCard>
-            <ProjectForm />
-            </div>
-    ));
-    return <div className="row">{items}</div>;
-
- }
-
- ProjectList.propTypes = {
-    projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
- };
-
-
+ProjectList.propTypes = {
+  projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+};
 
 export default ProjectList;
-
